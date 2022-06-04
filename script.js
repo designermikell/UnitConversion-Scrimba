04246c7672fc86b306d4f9f699a11c420f7ec7cc
Mike Mitchell <53986034+designermikell@users.noreply.github.com>
2022-06-04 00:03:17 +0000

    Add initial conversions script.

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
+let inpNumber = parseInt(document.getElementById("number").textContent); //converting string to number.
+let convLength = document.getElementById("convLength");
+let convVolume = document.getElementById("convVolume");
+let convMass = document.getElementById("convMass");
 
+ //Conversion for Length
+let ftToM = inpNumber / 3.281;
+let mToFt = inpNumber * 3.281;
+
+// Conversion for Volume
+let ltToGal = inpNumber * 1.201; 
+let galToLt = inpNumber / 1.201;
+
+// Conversion for Mass
+let kgToIbs = inpNumber * 2.2;
+let ibsToKg = inpNumber / 2.2;
+
+// Output Functions
+function convToLength(){
+convLength.textContent = `${inpNumber} meters = ${ftToM} feet | ${inpNumber} feet = ${mToFt} meters `
+}
+
+function convToVolume(){
+convVolume.textContent = `${inpNumber} liters = ${ltToGal} gallons | ${inpNumber} gallons = ${galToLt} liters`
+}
+
+function convToMass(){
+convMass.textContent = `${inpNumber} kilos = ${kgToIbs} pounds | ${inpNumber} pounds = ${ibsToKg} kilos`
+}
+
+convToLength();
+convToVolume();
+convToMass();
