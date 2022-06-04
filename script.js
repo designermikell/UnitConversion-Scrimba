3edddbdfdb2c5836e5de420436b3175cd750791e
Mike Mitchell <53986034+designermikell@users.noreply.github.com>
2022-06-04 01:29:25 +0000

    Add prompt for user input conversion.

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-let inpNumber = parseInt(document.getElementById("number").textContent); //converting string to number.
+let inpNumber = parseInt(prompt("Enter a number for conversion."));
+ //converting string to number.
+let covNumber = document.getElementById("number");
 let convLength = document.getElementById("convLength");
 let convVolume = document.getElementById("convVolume");
 let convMass = document.getElementById("convMass");
+
+
 
  //Conversion for Length
 let ftToM = inpNumber / 3.281;
@@ -17,14 +21,17 @@ let ibsToKg = inpNumber / 2.2;
 
 // Output Functions
 function convToLength(){
+covNumber.textContent = inpNumber;
 convLength.textContent = `${inpNumber} meters = ${ftToM.toFixed(3)} feet | ${inpNumber} feet = ${mToFt.toFixed(3)} meters `
 }
 
 function convToVolume(){
+covNumber.textContent = inpNumber;
 convVolume.textContent = `${inpNumber} liters = ${ltToGal.toFixed(3)} gallons | ${inpNumber} gallons = ${galToLt.toFixed(3)} liters`
 }
 
 function convToMass(){
+covNumber.textContent = inpNumber;
 convMass.textContent = `${inpNumber} kilos = ${kgToIbs.toFixed(3)} pounds | ${inpNumber} pounds = ${ibsToKg.toFixed(3)} kilos`
 }
 
